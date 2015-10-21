5253e11ad708f669d9513557fa08e17e29d54b50
Alan Williams <alanwill81@gmail.com>
2015-10-20 22:23:33 -0700

    fixing typo

--- zsh/install.sh
+++ zsh/install.sh
@@ -5,7 +5,7 @@
 # This checks for the presence of oh-my-zsh and if not present, installs it
 
 # Check for oh-my-zsh
-if [ ! -d "$HOME/.oh-my-zsh" ];
+if [ ! -d "$HOME"/.oh-my-zsh ];
 then
   echo "  Installing oh-my-zsh for you."
   sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
