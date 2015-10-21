569172c29e71209f62371fc753824eac81607cf5
Alan Williams <alanwill81@gmail.com>
2015-10-21 09:43:10 -0700

    Install bullet-train zsh theme

--- zsh/install.sh
+++ zsh/install.sh
@@ -9,6 +9,9 @@ if [ ! -d "$HOME"/.oh-my-zsh ];
 then
   echo "  Installing oh-my-zsh for you."
   sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  mkdir -p $HOME/.zshrc-custom/themes $HOME/.zshrc-custom/plugins
+  # install bullet-train theme
+  curl -O http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme $HOME/.zshrc-custom/themes/bullet-train.zsh-theme
 fi
 
 exit 0
