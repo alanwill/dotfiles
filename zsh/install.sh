7e18e02035e6afa2b6d3c71811f1b6963025f5b6
Alan Williams <alanwill81@gmail.com>
2015-10-21 09:58:28 -0700

    Fixing curl command

--- zsh/install.sh
+++ zsh/install.sh
@@ -9,9 +9,11 @@ if [ ! -d "$HOME"/.oh-my-zsh ];
 then
   echo "  Installing oh-my-zsh for you."
   sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  echo "  Creating custom directory for ZSH themes and plugins."
   mkdir -p $HOME/.zshrc-custom/themes $HOME/.zshrc-custom/plugins
-  # install bullet-train theme
-  curl -O http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme $HOME/.zshrc-custom/themes/bullet-train.zsh-theme
+  echo "  Installing bullet-train ZSH theme"
+  cd $HOME/.zshrc-custom/themes
+  curl -O http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme
 fi
 
 exit 0
