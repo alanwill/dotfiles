888ab7b1448da5d5c55996b4e5a5cd3f5c64fdbb
Alan Williams <alanwill81@gmail.com>
2015-10-21 21:15:27 -0700

    Python install

--- python/install.sh
+++ python/install.sh
@@ -1,1 +1,22 @@
-# Installation for pyenv
+#!/bin/sh
+#
+# Python installation
+#
+# This checks for the presence of pyenv and if not present, installs it
+# also installs both python 2 and 3 (superceeding the default python binaries in OS X)
+
+# Check for pyenv
+if test ! $(which pyenv)
+then
+  echo "*** Installing pyenv ***"
+  brew install pyenv
+  echo "*** Installing python 2.7.10 ***"
+  pyenv  install 2.7.10
+  echo "*** Installing python 2.7.10 ***"
+  pyenv  install 3.5.0
+  echo "*** Setting local python environment ***"
+  pyenv init -
+  pyenv local 2.7.10
+fi
+
+exit 0
