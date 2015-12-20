b739c36d8d9077c4167a8c0391120a5e3fab6921
Alan Williams <alanwill81@gmail.com>
2015-12-20 03:32:29 -0800

    Fixing tmuxinator window naming issues

--- system/exports.zsh
+++ system/exports.zsh
@@ -1,5 +1,9 @@
 export EDITOR='atom'
 
+# Tmuxinator
+export DISABLE_AUTO_TITLE=true
+
+# pyenv
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
